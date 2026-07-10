313e5fbff522de190ce161d4fd7717c8d0768cb2
Aryan Yadav <caryan830@gmail.com>
2026-07-10 20:42:35 +0530

    Hospital Managemnet System

---
+++ src/login.java
@@ -0,0 +1,11 @@
+    public class login {
+    
+        public static void main(String[] args) {
+            int a = 3;
+
+            for(int i = 1 ;i<=50;i++){
+                System.out.println(a +" * "+ i + " = "+(a *i));
+            }
+
+        }
+    }
